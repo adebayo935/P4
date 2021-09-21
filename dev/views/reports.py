44677d11de9aa4f42afc050ceb750b43de6febf3
adebayo935 <85680990+adebayo935@users.noreply.github.com>
2021-09-21 15:15:18 +0200

    Update reports.py

--- dev/views/reports.py
+++ dev/views/reports.py
@@ -2,17 +2,19 @@ import json
 from tinydb import TinyDB
 
 file = json.load(open("dev/db_tournaments.json"))
+db = TinyDB('dev/db_tournaments.json')
+tournaments_table = db.table('tournaments')
+tournaments = tournaments_table.all()
+players_table = db.table('players')
+players = players_table.all()
 
 
 def list_players():
+
     print("--- Liste des joueurs ---\n")
     print("1. Par classement\n")
     print("2. Par ordre alphabétique\n")
     choice = input("Votre choix : ")
-
-    db = TinyDB('dev/db_tournaments.json')
-    players_table = db.table('players')
-    players = players_table.all()
 
     if int(choice) == 1:
         sorted_players = sorted(players, key=lambda k: k['rank'])
@@ -33,41 +35,47 @@ def list_players():
 
 
 def list_players_tournament():
+
     print("--- Liste des joueurs par tournoi ---\n")
     choice = input("Entrez le nom du tournoi : ")
+    print("1. Par classement\n")
+    print("2. Par ordre alphabétique\n")
+    second_choice = input("Votre choix : ")
 
-    i = 1
-    while file['tournaments'][str(i)]:
-        if file['tournaments'][str(i)]['name'] == choice:
-            o = 1
-            for player in file['tournaments'][str(i)]['players']:
-                print('Player ' + str(o) + '\n' +
-                      'Prenom : ' + player['forename'] + '\n' +
-                      'Nom : ' + player['name'] + '\n' +
-                      'Date de naissance : ' + str(player['date']) + '\n' +
-                      'Sexe : ' + str(player['sex']) + '\n' +
-                      'Classement : ' + str(player['rank']) + '\n')
-                o += 1
-            break
-        else:
-            i += 1
+    for tournament in tournaments:
+        if choice == str(tournament['name']):
+            if int(second_choice) == 1:
+                sorted_players = sorted(tournament['players'], key=lambda k: k['rank'])
+                for player in sorted_players:
+                    print('Prenom : ' + str(player['forename']) + '\n' +
+                          'Nom : ' + str(player['name']) + '\n' +
+                          'Date de naissance : ' + str(player['date']) + '\n' +
+                          'Sexe : ' + str(player['sex']) + '\n' +
+                          'Classement : ' + str(player['rank']) + '\n')
+            elif int(second_choice) == 2:
+                sorted_players = sorted(tournament['players'], key=lambda k: k['forename'])
+                for player in sorted_players:
+                    print('Prenom : ' + str(player['forename']) + '\n' +
+                          'Nom : ' + str(player['name']) + '\n' +
+                          'Date de naissance : ' + str(player['date']) + '\n' +
+                          'Sexe : ' + str(player['sex']) + '\n' +
+                          'Classement : ' + str(player['rank']) + '\n')
 
 
 def list_tournaments():
     print("--- Liste de tous les tournois ---\n")
-
     i = 1
-    while file['tournaments'][str(i)]:
+    for tournament in tournaments:
         print('Tournoi ' + str(i) + '\n' +
-              'Nom : ' + str(file['tournaments'][str(i)]['name']) + '\n' +
-              'Lieu : ' + str(file['tournaments'][str(i)]['place']) + '\n' +
-              'Temps : ' + str(file['tournaments'][str(i)]['time']) + '\n' +
-              'Description: ' + str(file['tournaments'][str(i)]['description']) + '\n\n' +
+              'Nom : ' + str(tournament['name']) + '\n' +
+              'Lieu : ' + str(tournament['place']) + '\n' +
+              'Temps : ' + str(tournament['time']) + '\n' +
+              'Description: ' + str(tournament['description']) + '\n\n' +
               'Liste des Joueurs :')
         o = 1
-        for player in file['tournaments'][str(i)]['players']:
+        for player in tournament['players']:
             print('Player ' + str(o) + '\n' +
-                  'Prenom : ' + str(player['forename']) + '\n' +
+                  'Prénom : ' + str(player['forename']) + '\n' +
                   'Nom : ' + str(player['name']) + '\n' +
                   'Date de naissance : ' + str(player['date']) + '\n' +
                   'Sexe : ' + str(player['sex']) + '\n' +
@@ -75,7 +83,7 @@ def list_tournaments():
             o += 1
         print('Liste des tours :')
         o = 1
-        for tour in file['tournaments'][str(i)]['rounds']:
+        for tour in tournament['rounds']:
             print('Tour  ' + str(o) + '\n' +
                   'Date de début : ' + str(tour['date debut']) + '\n' +
                   'Date de fin : ' + str(tour['date fin']) + '\n' +
@@ -91,19 +99,17 @@ def list_tournaments():
                 s += 1
             o += 1
         i += 1
-        break
 
 
 def list_rounds_tournament():
     print("--- Liste des tours d'un tournoi ---\n")
     choice = input("Entrez le nom du tournoi : ")
 
-    i = 1
-    while file['tournaments'][str(i)]:
-        if file['tournaments'][str(i)]['name'] == choice:
+    for tournament in tournaments:
+        if choice == str(tournament['name']):
             print('Liste des tours :')
             o = 1
-            for tour in file['tournaments'][str(i)]['rounds']:
+            for tour in tournament['rounds']:
                 print('Tour  ' + str(o) + '\n' +
                       'Date de début : ' + str(tour['date debut']) + '\n' +
                       'Date de fin : ' + str(tour['date fin']) + '\n' +
@@ -118,20 +124,16 @@ def list_rounds_tournament():
                           'Score 1 : ' + str(game['Score P1']) + '\n' +
                           'Score 2 : ' + str(game['Score P2']) + '\n')
                     s += 1
-            break
-        else:
-            i += 1
 
 
 def list_games_tournament():
     print("--- Liste des matchs d'un tournoi ---\n")
     choice = input("Entrez le nom du tournoi : ")
 
-    i = 1
-    while file['tournaments'][str(i)]:
-        if file['tournaments'][str(i)]['name'] == choice:
+    for tournament in tournaments:
+        if choice == str(tournament['name']):
             o = 1
-            for tour in file['tournaments'][str(i)]['rounds']:
+            for tour in tournament['rounds']:
                 print('Tour  ' + str(o) + '\n')
                 o += 1
                 s = 1
@@ -142,6 +144,3 @@ def list_games_tournament():
                           'Score 1 : ' + str(game['Score P1']) + '\n' +
                           'Score 2 : ' + str(game['Score P2']) + '\n')
                     s += 1
-            break
-        else:
-            i += 1
